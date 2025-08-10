87d55d43c4c2d70ab9a3176958bd1435e429cd7b
Muhammad Sulaiman <218628972+mk-slmn@users.noreply.github.com>
2025-08-10 16:57:12 +0100

    add recoverer and logger middleware

--- services/api/handlers/server.go
+++ services/api/handlers/server.go
@@ -1,9 +1,17 @@
 package handlers
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func NewServer() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	r.Get("/v1/health", Health)
+
 	return r
 }
