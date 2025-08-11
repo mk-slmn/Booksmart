450780cc426e8f782fdad789aec49b5022e58079
Muhammad Sulaiman <218628972+mk-slmn@users.noreply.github.com>
2025-08-11 13:21:12 +0100

    add helpers (json, tx wrapper, time)

---
+++ services/api/handlers/utli.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func writeJSON(w http.ResponseWriter, code int, v any) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	_ = json.NewEncoder(w).Encode(v)
+}
+
+func writeErr(w http.ResponseWriter, code int, msg string) {
+	writeJSON(w, code, map[string]any{
+		"error": map[string]any{
+			"code":    http.StatusText(code),
+			"message": msg,
+		},
+	})
+}
+
+func withTx(db *sql.DB, fn func(*sql.Tx) error) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	if err := fn(tx); err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+func parseRFC3339UTC(s string) (time.Time, error) {
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return t.UTC(), nil
+}
+
+func timeOrNowRFC3339(t *time.Time) string {
+	if t == nil {
+		return time.Now().Format(time.RFC3339)
+	}
+	return t.UTC().Format(time.RFC3339)
+}
